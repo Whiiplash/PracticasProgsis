bdd1a4b5b3d51350176e0b8e0e63da7a88ae5550
Whiiplash <solamenteporpensar@hotmail.com>
2013-10-16 15:55:18 -0500

    Re restored

--- Practica 0/ProgSist/MainForm.cs
+++ Practica 0/ProgSist/MainForm.cs
@@ -9,7 +9,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 using System.IO;
-/// Jala Chido
+
 namespace ProgSist
 {
 	/// <summary>
@@ -40,22 +40,8 @@ namespace ProgSist
 			//ABRIR INICIO
 			OpenFileDialog openfile = new OpenFileDialog();
 			openfile.Title = "Seleccione un archivo";
-			openfile.Filter = "Archivos ASM|*.asm|Archivos TXT|*.txt|Archivos TODOS|*.*";
 			if (openfile.ShowDialog() == DialogResult.OK) 
 				{
-<<<<<<< HEAD
-					this.laruta.Text = openfile.FileName; 
-					
-			System.IO.StreamReader sreader = new System.IO.StreamReader(@laruta.Text, System.Text.Encoding.Default);
-			String txto;
-            txto = sreader.ReadToEnd();
-            txtbox.Text = txto;
-			sreader.Close();
-			
-				}
-		
-			else if (openfile.ShowDialog() == DialogResult.Cancel){}
-=======
 					this.laruta.Text= openfile.FileName; 
 					System.IO.StreamReader sreader = new System.IO.StreamReader(@laruta.Text, System.Text.Encoding.Default);
 					
@@ -69,15 +55,12 @@ namespace ProgSist
 					sreader.Close();
 					
 				}
->>>>>>> c6bc58a216efab6e3fabf0ded5efcb9de3b93811
 			//ABRIR FIN
 		}
 		
 		void BotonClick(object sender, EventArgs e)
-		{	
-			/*string [] lines = this.txtbox.Text.Split('\n');
-			MessageBox.Show(lines.Length.ToString());*/
-			//Close();
+		{
+			Close();
 		}
 	}
 }
